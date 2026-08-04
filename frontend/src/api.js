8650769424aead6203b97d569a35bc8f848ce390
jithender <jithender2003b@gmail.com>
2026-08-04 10:25:50 +0530

    Update backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://event-ticket-booking-system-zbd5.onrender.com",
 })
 
-export default api 
+export default api  
